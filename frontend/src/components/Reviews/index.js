a83e217f832f58a5c0e8ea9e91c12502183ae8af
enomilan <andrewmusta@gmail.com>
2021-07-25 22:21:46 -0500

    review

---
+++ frontend/src/components/Reviews/index.js
@@ -0,0 +1,13 @@
+import React, {useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import * as placeReducer from '../../store/places'
+
+function Reviews() {
+    const dispatch = useDispatch()   
+
+
+    return(
+        <div></div>
+    )
+
+}
